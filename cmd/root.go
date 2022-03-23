76f210a822286a4f76ec60cdab8c0999fd131c46
Jed <bibatjed@gmail.com>
2022-03-23 18:01:59 +0800

    chore: remove unused code

--- cmd/root.go
+++ cmd/root.go
@@ -9,10 +9,6 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "ip",
 	Long:  `A cli that can track ip location`,
-	//Run: func(cmd *cobra.Command, args []string) {
-	//	// Do Stuff Here
-	//	action.TrackIP(args[0])
-	//},
 }
 
 func Execute() {
